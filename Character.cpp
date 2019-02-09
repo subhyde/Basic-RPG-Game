22a8d3fb1abaa7f888f0400cf416e65863dd9238
subhyde <ethan_johnson@hotmail.ca>
2019-02-08 22:37:49 -0500

    Update Character.cpp
    added gold
    random item drops a wip

--- Character.cpp
+++ Character.cpp
@@ -4,88 +4,88 @@
 #include "Windows.h"
 using namespace std;
 
-
-bool Character::Battle(Character Monster, short level)
+bool User::Battle(Character Monster, short level)
 {
     srand(time(NULL));
     bool win = false;
 
-    int aori = 1;
-    cout << "press 1 to attack: ";
-    cin >> aori;
-
-
     while (this->HP > 0 || Monster.HP > 0)
     {
-///////////////////////////////////////////////////////
-        while (true){
-            if( aori == 1){
-                aori = 0;
+        int option = 1;
+        cout << "1) Attack\t2) Inventory ";
+        cin >> option;
+        cout << endl;
+        ///////////////////////////////////////////////////////
+
+        if (option == 2)
+        {
+            User::displayInventory();
+            continue;
+        }
+
+        else if (option == 1)
+        {
+
+            ///////////////////////////////////////////////////////
+
+            short damage = rand() % this->DAMAGE + 1;
+            Monster.HP -= damage + Monster.ARMOR;
+            cout << "You did " << damage + Monster.ARMOR << " damage!" << endl;
+            if (damage == 0)
+                cout << "You Missed Idiot." << endl;
+            //Sleep(1000);
+            if (Monster.HP <= 0)
+            {
+                cout << "You have killed " << Monster.NAME << " " << level + 1 << "!" << endl
+                     << endl;
+                cout << "You have obtained a health potion!" << endl;
+                bool obtained = editInventory("health potion");
+                cout << "Get ready for the next monster. He will be tougher." << endl;
+                //Sleep(1000);
+                win = true;
                 break;
             }
 
-            else if ( aori == 2){
-
-                cout << inventory[1][0]<< inventory[1][1];
+            damage = rand() % Monster.DAMAGE + 1;
+            this->HP -= damage + this->ARMOR;
+            cout << Monster.NAME << " " << level + 1 << " did " << damage + this->ARMOR << " damage!" << endl
+                 << endl;
+            if (damage == 0)
+                cout << "LUL it missed!!!!!!!!! lulululululul!" << endl;
+            //Sleep(1000);
+            if (this->HP <= 0)
+            {
+                cout << Monster.NAME << " " << level + 1 << " has killed you!" << endl
+                     << endl;
+                //Sleep(1000);
+                win = false;
+                break;
             }
-            aori = 0;
-            cout << "press 1 to attack";
-            cin >> aori;
 
-
+            if (this->HP <= 0)
+            {
+                cout << "Your HP: 0" << '\t' << Monster.NAME << " " << level + 1 << " HP: " << Monster.HP << "." << endl;
+                cout << "-------------------------------------------------------" << endl
+                     << endl;
+                break;
+            }
+            else if (Monster.HP <= 0)
+            {
+                cout << "Your HP: " << this->HP << '\t' << Monster.NAME << " " << level + 1 << " HP: 0." << endl;
+                cout << "-------------------------------------------------------" << endl
+                     << endl;
+            }
+            else
+            {
+                cout << "Your HP: " << this->HP << '\t' << Monster.NAME << " " << level + 1 << " HP: " << Monster.HP << "." << endl;
+                cout << "-------------------------------------------------------" << endl
+                     << endl;
+            }
+            //Sleep(1000);
         }
-///////////////////////////////////////////////////////
-
-        short damage = rand() % this->DAMAGE + 1;
-        Monster.HP -= damage + Monster.ARMOR;
-        cout << "You did " << damage + Monster.ARMOR << " damage!" << endl;
-        if (damage == 0)
-            cout << "You Missed Idiot." << endl;
-        Sleep(1000);
-        if (Monster.HP <= 0)
-        {
-            cout << "You have killed " << Monster.NAME << " " << level + 1 << "!" << endl << endl;
-            Sleep(1000);
-            win = true;
-            break;
-        }
-
-        damage = rand() % Monster.DAMAGE + 1;
-        this->HP -= damage + this->ARMOR;
-        cout << Monster.NAME << " " << level + 1 << " did " << damage + this->ARMOR << " damage!" << endl;
-        if (damage == 0)
-            cout << "LUL it missed!!!!!!!!! lulululululul!" << endl;
-        Sleep(1000);
-        if (this->HP <= 0)
-        {
-            cout << endl << Monster.NAME << " " << level + 1 << " has killed you!" << endl
-                 << endl;
-            Sleep(1000);
-            win = false;
-            break;
-        }
-
-        if (this->HP <= 0)
-        {
-            cout << "Your HP: 0" << '\t' << Monster.NAME << " " << level + 1 <<" HP: " << Monster.HP << "." << endl;
-            cout << "-------------------------------------------------------" << endl
-                 << endl;
-            break;
-        }
-        else if (Monster.HP <= 0)
-        {
-            cout << "Your HP: " << this->HP << '\t' << Monster.NAME << " " << level + 1 << " HP: 0." << endl;
-            cout << "-------------------------------------------------------" << endl
-                 << endl;
-        }
-        else
-        {
-            cout << "Your HP: " << this->HP << '\t' << Monster.NAME << " " << level + 1 << " HP: " << Monster.HP << "." << endl;
-            cout << "-------------------------------------------------------" << endl
-                 << endl;
-        }
-        Sleep(1000);
     }
+    afterBattle();
+    cout << "You got " << GOLDCOLLECT << " gold from winning the battle!\nYou have a total of: " << GOLD << " Gold!" <<endl;
     return win;
 }
 
@@ -97,15 +97,84 @@ Character::Character()
     this->DAMAGE = 10;
     this->ARMOR = 0;
 }
-Character::Character(short playerLVL)
+Character::Character(short LVL)
 {
     this->NAME = "Stronger Monster";
-    this->LVL = playerLVL;
+    this->LVL = LVL;
     this->HP = 100;
-    this->DAMAGE = 10 * playerLVL * .75;
-    this->ARMOR = playerLVL;
+    this->DAMAGE = 10 * LVL * .75;
+    this->ARMOR = LVL;
 }
 
-User::User() {
-    this->EXP = 0;
+User::User()
+{
+    this->LVL = 0;
+}
+
+void User::displayInventory()
+{
+    for (int item = 0; item < 4; item++)
+        cout << this->INVENTORY[0][item] << "(" << this->INVENTORY[1][item] << ")" << endl
+             << endl;
+}
+
+bool User::editInventory(string item)
+{
+    if (this->INVENTORY[0][3].length() == 0)
+        return false;
+
+    bool match = false;
+    for(int i = 0; i < 4; i++)
+    {
+        if(item == this->INVENTORY[0][i])
+        {
+            short temp = stoi(this->INVENTORY[1][i]);
+            this->INVENTORY[1][i] = to_string(++temp);
+            match = true;
+            break;
+        }
+    }
+    if(!match)
+    {
+        if(this->INVENTORY[0][3].length() != 0)
+            return false;
+        for(int i = 0; i < 4; i++)
+        {
+            if(this->INVENTORY[0][i].length() == 0)
+            {
+                this->INVENTORY[0][i] = item;
+                short temp = stoi(this->INVENTORY[1][i]);
+                this->INVENTORY[1][i] = to_string(++temp);
+            }
+        }
+    }
+    cout << "*" << item << " has been added to your inventory*" << endl << endl;
+    return true;
+}
+
+void User::prepareNextRound()
+{
+    this->HP = 100;
+    this->LVL++;
+}
+
+short User::retrievePlayerLevel() {return this->LVL;}
+
+void User::afterBattle() {
+
+    GOLDCOLLECT += LVL * 2;
+    GOLD += GOLDCOLLECT;
+
+
+    //for item drops
+
+
+
+    int healthDrop = rand() % 2;
+    if (healthDrop == 1) {
+        int stringtoint = stoi(INVENTORY[0][1]);
+        stringtoint++;
+        INVENTORY[1][0] = to_string(stringtoint);
+
+    }
 }
