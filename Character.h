3c59dce05384bbbb6ac66e2f1b378272d355c74f
subhyde <ethan_johnson@hotmail.ca>
2019-02-08 22:39:16 -0500

    Update Character.h
    Added gold members and after battle function

--- Character.h
+++ Character.h
@@ -3,25 +3,33 @@ using namespace std;
 
 class Character
 {
+protected:
     string NAME;
     short LVL;
     float HP;
     float ARMOR;
     short DAMAGE;
 
-    public:
-        bool Battle(Character);
-        Character();
-        Character(short);
-        string inventory[4][4]= {"health potion", "damage boost potion", " shield potion", "instakill potion",0,0,0,0};
-
+public:
+    Character();
+    Character(short);
+    friend class User;
 };
 
 class User : public Character
 {
-    short EXP;
     short KILLS;
+    string INVENTORY[4][4] = {{"health potion", "damage boost potion", "shield potion", "instakill potion"}, {"0", "0", "0", "0"}};
+    int GOLD;
+    int GOLDCOLLECT;
+public:
+    User();
+    bool Battle(Character, short);
+    void displayInventory();
+    bool editInventory(string);
+    void prepareNextRound();
+    short retrievePlayerLevel();
+    void afterBattle();
 
-    public:
-        User();
+
 };
