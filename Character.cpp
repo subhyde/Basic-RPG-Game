1295c839de1a8e92d1aa1f6b6e8ec2e007a35e10
subhyde <ethan_johnson@hotmail.ca>
2019-02-11 09:07:54 -0500

    update 5 | added a shop and gold collection
    as well as a working health upgrade

--- Character.cpp
+++ Character.cpp
@@ -4,27 +4,28 @@
 #include "Windows.h"
 using namespace std;
 
-bool User::Battle(Character Monster, short level)
-{
+bool User::Battle(Character Monster, short level) {
     srand(time(NULL));
     bool win = false;
 
-    while (this->HP > 0 || Monster.HP > 0)
-    {
+    while (this->HP > 0 || Monster.HP > 0) {
         int option = 1;
-        cout << "1) Attack\t2) Inventory ";
+        cout << "1) Attack\t2) Inventory\t3) Shop ";
         cin >> option;
         cout << endl;
         ///////////////////////////////////////////////////////
 
-        if (option == 2)
-        {
-            User::displayInventory();
+        if (option == 3){
+
+            shop();
             continue;
         }
 
-        else if (option == 1)
-        {
+        else if (option == 2) {
+            User::displayInventory();
+            continue;
+
+        } else if (option == 1) {
 
             ///////////////////////////////////////////////////////
 
@@ -33,15 +34,14 @@ bool User::Battle(Character Monster, short level)
             cout << "You did " << damage + Monster.ARMOR << " damage!" << endl;
             if (damage == 0)
                 cout << "You Missed Idiot." << endl;
-            //Sleep(1000);
-            if (Monster.HP <= 0)
-            {
+            Sleep(1000);
+            if (Monster.HP <= 0) {
                 cout << "You have killed " << Monster.NAME << " " << level + 1 << "!" << endl
                      << endl;
                 cout << "You have obtained a health potion!" << endl;
                 bool obtained = editInventory("health potion");
                 cout << "Get ready for the next monster. He will be tougher." << endl;
-                //Sleep(1000);
+                Sleep(1000);
                 win = true;
                 break;
             }
@@ -52,53 +52,48 @@ bool User::Battle(Character Monster, short level)
                  << endl;
             if (damage == 0)
                 cout << "LUL it missed!!!!!!!!! lulululululul!" << endl;
-            //Sleep(1000);
-            if (this->HP <= 0)
-            {
+            Sleep(1000);
+            if (this->HP <= 0) {
                 cout << Monster.NAME << " " << level + 1 << " has killed you!" << endl
                      << endl;
-                //Sleep(1000);
+                Sleep(1000);
                 win = false;
                 break;
             }
 
-            if (this->HP <= 0)
-            {
-                cout << "Your HP: 0" << '\t' << Monster.NAME << " " << level + 1 << " HP: " << Monster.HP << "." << endl;
+            if (this->HP <= 0) {
+                cout << "Your HP: 0" << '\t' << Monster.NAME << " " << level + 1 << " HP: " << Monster.HP << "."
+                     << endl;
                 cout << "-------------------------------------------------------" << endl
                      << endl;
                 break;
-            }
-            else if (Monster.HP <= 0)
-            {
+            } else if (Monster.HP <= 0) {
                 cout << "Your HP: " << this->HP << '\t' << Monster.NAME << " " << level + 1 << " HP: 0." << endl;
                 cout << "-------------------------------------------------------" << endl
                      << endl;
-            }
-            else
-            {
-                cout << "Your HP: " << this->HP << '\t' << Monster.NAME << " " << level + 1 << " HP: " << Monster.HP << "." << endl;
+            } else {
+                cout << "Your HP: " << this->HP << '\t' << Monster.NAME << " " << level + 1 << " HP: " << Monster.HP
+                     << "." << endl;
                 cout << "-------------------------------------------------------" << endl
                      << endl;
             }
-            //Sleep(1000);
+            Sleep(1000);
         }
     }
-    afterBattle();
-    cout << "You got " << GOLDCOLLECT << " gold from winning the battle!\nYou have a total of: " << GOLD << " Gold!" <<endl;
+
     return win;
 }
 
-Character::Character()
-{
+Character::Character() {
     this->NAME = "Monster";
     this->LVL = 1;
     this->HP = 100;
-    this->DAMAGE = 10;
+    this->DAMAGE = 200;
     this->ARMOR = 0;
+
 }
-Character::Character(short LVL)
-{
+
+Character::Character(short LVL) {
     this->NAME = "Stronger Monster";
     this->LVL = LVL;
     this->HP = 100;
@@ -106,42 +101,36 @@ Character::Character(short LVL)
     this->ARMOR = LVL;
 }
 
-User::User()
-{
+User::User() {
     this->LVL = 0;
+    this->GOLD = 0;
+    this->GOLDCOLLECT = 0;
 }
 
-void User::displayInventory()
-{
+void User::displayInventory() {
     for (int item = 0; item < 4; item++)
         cout << this->INVENTORY[0][item] << "(" << this->INVENTORY[1][item] << ")" << endl
              << endl;
 }
 
-bool User::editInventory(string item)
-{
+bool User::editInventory(string item) {
     if (this->INVENTORY[0][3].length() == 0)
         return false;
 
     bool match = false;
-    for(int i = 0; i < 4; i++)
-    {
-        if(item == this->INVENTORY[0][i])
-        {
+    for (int i = 0; i < 4; i++) {
+        if (item == this->INVENTORY[0][i]) {
             short temp = stoi(this->INVENTORY[1][i]);
             this->INVENTORY[1][i] = to_string(++temp);
             match = true;
             break;
         }
     }
-    if(!match)
-    {
-        if(this->INVENTORY[0][3].length() != 0)
+    if (!match) {
+        if (this->INVENTORY[0][3].length() != 0)
             return false;
-        for(int i = 0; i < 4; i++)
-        {
-            if(this->INVENTORY[0][i].length() == 0)
-            {
+        for (int i = 0; i < 4; i++) {
+            if (this->INVENTORY[0][i].length() == 0) {
                 this->INVENTORY[0][i] = item;
                 short temp = stoi(this->INVENTORY[1][i]);
                 this->INVENTORY[1][i] = to_string(++temp);
@@ -152,17 +141,16 @@ bool User::editInventory(string item)
     return true;
 }
 
-void User::prepareNextRound()
-{
-    this->HP = 100;
+void User::prepareNextRound() {
+    this->HP = 100 + this->Healthbonus;
     this->LVL++;
 }
 
-short User::retrievePlayerLevel() {return this->LVL;}
+short User::retrievePlayerLevel() { return this->LVL; }
 
 void User::afterBattle() {
 
-    GOLDCOLLECT += LVL * 2;
+    GOLDCOLLECT += LVL * 10;
     GOLD += GOLDCOLLECT;
 
 
@@ -170,11 +158,52 @@ void User::afterBattle() {
 
 
 
-    int healthDrop = rand() % 2;
-    if (healthDrop == 1) {
-        int stringtoint = stoi(INVENTORY[0][1]);
-        stringtoint++;
-        INVENTORY[1][0] = to_string(stringtoint);
+   // int healthDrop = rand() % 2;
+ //   if (healthDrop == 1) {
+  //      int stringtoint = stoi(INVENTORY[0][1]);
+    //    stringtoint++;
+    //    INVENTORY[0][0] = to_string(stringtoint);
 
+ //   }
+}
+
+
+
+void User::shop(){
+    int shopOption;
+    cout << "Welcome to my shop young warrier!\nWhat are you looking for?" << endl;
+
+    while (true) {
+        cout << "[1] health increase\t 20 gold" << endl;
+        cout << "You currently have [" << GOLDCOLLECT <<  "] gold" << endl;
+        cout << "press [0] to quit"<< endl;
+        cin >> shopOption;
+        if (shopOption == 0){
+            break;
+        }
+
+        if (shopOption == 1) {
+            if (GOLDCOLLECT >= 20) {
+                cout << "health successfully increased" << endl;
+                GOLDCOLLECT -= 20;
+                this-> Healthbonus += 20;
+                cout << "you currently have:" << this->Healthbonus + this->HP << " HP" << endl;
+                this->HP += this->Healthbonus;
+
+
+            }
+            else{
+
+                cout << "you don't have enough gold" << endl;
+            }
+
+
+
+        }
     }
+
+
+
+
+
 }
